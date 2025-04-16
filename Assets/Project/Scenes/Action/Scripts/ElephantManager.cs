e6966262789b8f42d0bc1a8714f8ed43b9c4dd1e
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2025-04-17 01:26:00 +0900

    :sparkles: Cinemachineを導入 + カメラが回るように

--- Assets/Project/Scenes/Action/Scripts/ElephantManager.cs
+++ Assets/Project/Scenes/Action/Scripts/ElephantManager.cs
@@ -3,20 +3,21 @@ using UnityEngine;
 public class ElephantManager : MonoBehaviour
 {
     // TODO: 後でScriptable Objectに修正したい
-    private readonly float MIN_JUMP_POWER = 100f;
-    private readonly float MAX_JUMP_POWER = 300f;
-    private readonly float CHARGE_RATE = 600f;
+    readonly float MIN_JUMP_POWER = 100f;
+    readonly float MAX_JUMP_POWER = 300f;
+    readonly float CHARGE_RATE = 600f;
 
-    private readonly float GROUND_MOVE_POWER = 0.5f;
-    private readonly float AIR_MOVE_POWER = 0.2f;
+    readonly float GROUND_MOVE_POWER = 0.5f;
+    readonly float AIR_MOVE_POWER = 0.2f;
 
-    private readonly float GROUND_SPIN_POWER = 100f;
-    private readonly float AIR_MAX_SPIN_SPEED = 3f;
-    private readonly float AIR_SPIN_POSER = 1f;
+    readonly float GROUND_SPIN_POWER = 100f;
+    readonly float AIR_MAX_SPIN_SPEED = 3f;
+    readonly float AIR_SPIN_POSER = 1f;
 
-    private Rigidbody _rigidbody;
-    private float _chargeStartTime = 0.0f;
-    private bool _isGround = false;
+    Rigidbody _rigidbody;
+
+    float _chargeStartTime = 0.0f;
+    bool _isGround = false;
 
     void Start()
     {
@@ -28,7 +29,8 @@ public class ElephantManager : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(h, 0, v).normalized;
+        Quaternion rotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        Vector3 velocity = rotation * new Vector3(h, 0, v).normalized;
 
         if (_isGround)
         {
@@ -43,12 +45,11 @@ public class ElephantManager : MonoBehaviour
                 float jumpPower = Mathf.Clamp(chargeTime * CHARGE_RATE, MIN_JUMP_POWER, MAX_JUMP_POWER);
 
                 // Debug.Log(jumpPower);
+                _rigidbody.AddForce((Vector3.up + velocity * GROUND_MOVE_POWER) * jumpPower, ForceMode.Acceleration);
 
-                _rigidbody.AddForce((Vector3.up + direction * GROUND_MOVE_POWER) * jumpPower, ForceMode.Acceleration);
-
-                if (direction != Vector3.zero)
+                if (velocity != Vector3.zero)
                 {
-                    Vector3 spinAxis = Vector3.Cross(Vector3.up, direction);
+                    Vector3 spinAxis = Vector3.Cross(Vector3.up, velocity);
                     _rigidbody.AddTorque(spinAxis * GROUND_SPIN_POWER, ForceMode.Acceleration);
                 }
             }
@@ -56,16 +57,16 @@ public class ElephantManager : MonoBehaviour
 
         if (!_isGround)
         {
-            if (direction != Vector3.zero)
+            if (velocity != Vector3.zero)
             {
-                _rigidbody.AddForce(direction * AIR_MOVE_POWER, ForceMode.Acceleration);
+                _rigidbody.AddForce(velocity * AIR_MOVE_POWER, ForceMode.Acceleration);
             }
         }
 
         // Debug.Log(_rigidbody.angularVelocity.magnitude);
         if (_rigidbody.angularVelocity.magnitude < AIR_MAX_SPIN_SPEED)
         {
-            Vector3 spinAxis = Vector3.Cross(Vector3.up, direction);
+            Vector3 spinAxis = Vector3.Cross(Vector3.up, velocity);
             _rigidbody.AddTorque(spinAxis * AIR_SPIN_POSER, ForceMode.Acceleration);
         }
     }
